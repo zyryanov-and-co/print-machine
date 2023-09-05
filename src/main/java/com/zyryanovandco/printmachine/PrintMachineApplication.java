f29cedf20f292321416d3ac2a6b300833f8b08e7
Aleksey Zyryanov <cerfscode@gmail.com>
2023-09-05 19:47:14 +0300

    init

---
+++ src/main/java/com/zyryanovandco/printmachine/PrintMachineApplication.java
@@ -0,0 +1,13 @@
+package com.zyryanovandco.printmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zyryanovandco/printmachine/PrintMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zyryanovandco.printmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrintMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
